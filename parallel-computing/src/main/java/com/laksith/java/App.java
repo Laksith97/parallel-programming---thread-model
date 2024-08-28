d42551ceab25f78532a2a932b6f41a9fc8123717
Laksith97 <laksithrathnayake456@gmail.com>
2024-08-28 15:32:14 +0530

    added thread execution method - exeThreads()

--- parallel-computing/src/main/java/com/laksith/java/App.java
+++ parallel-computing/src/main/java/com/laksith/java/App.java
@@ -3,19 +3,24 @@ package com.laksith.java;
 public class App {
     public static void main(String[] args) {
 
+        exeThreads();
+    }
+
+    public static void exeThreads() {
         for (int i = 0; i < 10; i++) {
             final int index = i;
             // new Thread(new Runnable() {
-            //     public void run() {
-            //         System.out.println("Current Thread id: " + Thread.currentThread().getId());
-            //     }
+            // public void run() {
+            // System.out.println("Current Thread id: " + Thread.currentThread().getId());
+            // }
             // }).start();
 
             // Runnable runnable = new Runnable() {
-            //     @Override
-            //     public void run() {
-            //         System.out.println("Current Thread id: " + Thread.currentThread().getId() + ", Index: " + index);
-            //     }
+            // @Override
+            // public void run() {
+            // System.out.println("Current Thread id: " + Thread.currentThread().getId() +
+            // ", Index: " + index);
+            // }
             // };
             // new Thread(runnable).start();
 
