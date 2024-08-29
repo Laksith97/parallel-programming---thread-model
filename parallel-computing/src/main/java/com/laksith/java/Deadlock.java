487754fd76c39e646256678734f27195be9748b7
Laksith97 <laksithrathnayake456@gmail.com>
2024-08-29 10:07:42 +0530

    created thread 1 and lock resources

--- parallel-computing/src/main/java/com/laksith/java/Deadlock.java
+++ parallel-computing/src/main/java/com/laksith/java/Deadlock.java
@@ -1,15 +1,34 @@
 package com.laksith.java;
 
 public class Deadlock {
+
     public static void main(String[] args) {
         //System.out.println("Deadlock!");
+        final String res1 = "res1";
+        final String res2 = "res2";
 
         Thread t1 = new Thread(new Runnable() {
 
             @Override
             public void run() {
+                // to lock resource 1: Allocate r1 for thread 1
+                // so, t1 owns r1
+                synchronized (res1) {
+                    System.out.println("Locked on res1");
+                }
+
+                // sleep res1 for 100 milliseconds
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 
-                throw new UnsupportedOperationException("Unimplemented method 'run'");
+                // then try to lock res2
+                synchronized (res2) {
+                    System.out.println("Locked on res2");
+                }
+                
             }
             
         })
